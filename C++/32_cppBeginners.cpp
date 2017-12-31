58a1cfe1a7afa625360ebc1319eb8e34508df505
Tlhbobby <tlhbobby@gmail.com>
2017-12-30 20:52:30 -0500

    Updated bucky [1] = 2 to bucky [2] = 2

--- C++/32_cppBeginners.cpp
+++ C++/32_cppBeginners.cpp
@@ -5,5 +5,5 @@ int main()
 {
     int bucky[5] = {66,75,2,43,99}; // array initialiser list
 
-    cout << bucky[2] << endl; // the array index starts at 0 so bucky[0] = 66, bucky[1] = 75, and bucky[1] = 2 ...
+    cout << bucky[2] << endl; // the array index starts at 0 so bucky[0] = 66, bucky[1] = 75, and bucky[2] = 2 ...
 }
